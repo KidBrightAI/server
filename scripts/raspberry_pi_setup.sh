990ebf3e75bed0c5af86f817782577e50188ebe5
comdet <listzone@hotmail.com>
2022-08-19 12:13:53 +0700

    update rpi

--- scripts/raspberry_pi_setup.sh
+++ scripts/raspberry_pi_setup.sh
@@ -1,7 +1,7 @@
 cd
 git clone https://github.com/KidBrightAI/server kbai-server
 cd kbai-server
-sudo raspi-config ->extend partion
+#sudo raspi-config ->extend partion
 pip3 install https://github.com/bitsy-ai/tensorflow-arm-bin/releases/download/v2.4.0-rc2/tensorflow-2.4.0rc2-cp37-none-linux_armv7l.whl
 pip3 install https://github.com/bitsy-ai/tensorflow-arm-bin/releases/download/v2.4.0/tensorflow-2.4.0-cp37-none-linux_armv7l.whl
 pip3 install https://github.com/lhelontra/tensorflow-on-arm/releases/download/v2.4.0/tensorflow-2.4.0-cp37-none-linux_armv7l.whl
