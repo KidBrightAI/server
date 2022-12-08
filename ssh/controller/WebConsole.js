30d13e3cb64da45f14f96a9d97733c958c70defe
comdet <listzone@hotmail.com>
2022-12-09 00:05:22 +0700

    update

--- ssh/controller/WebConsole.js
+++ ssh/controller/WebConsole.js
@@ -16,26 +16,20 @@ const onConnect = async (ws, req) => {
   await ssh.connect(sshConfig);
   shellStream = await ssh.requestShell();
   ws.on("message", (msg) => {
-    const data = JSON.parse(msg);
-    switch (data.method) {
-      case "command":
-        shellStream.write(data.command.trim() + "\n");
-        break;
+    if (msg == "CMD:TERM") {
+      shellStream.write("\x03");
+      shellStream.write("\x03");
+      shellStream.write("\n");
+      shellStream.write("\n");
+    } else {
+      shellStream.write(msg);
     }
   });
   // listener
   shellStream.on("data", (data) => {
-    // const d = JSON.stringify({
-    //   jsonrpc: "2.0",
-    //   data: data.toString(),
-    // });
     ws.send(data.toString());
   });
   shellStream.stderr.on("data", (data) => {
-    // const d = JSON.stringify({
-    //   jsonrpc: "1.0",
-    //   data: data.toString(),
-    // });
     ws.send(d.toString());
   });
 };
