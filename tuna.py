65a81c13e29bf4d560edab0de543f51ce1c5c22b
Ridnarong Promya <ridnarongp@yahoo.com>
2023-01-25 20:50:13 +0700

    Add Tuna Tunnel

---
+++ tuna.py
@@ -0,0 +1,25 @@
+import shlex, subprocess
+import sys
+
+PYTHON_DEFAULT = 'python'+str(sys.version_info[0])
+SETTING = {
+  'DOMAIN': 'wot.mecacloud.top'
+}
+
+def run_tuna(port,token):
+  tokens = token.split(':')
+  if len(tokens) == 2:
+    frontend = " --frontend="+tokens[0]+":80"
+    service_on = " --service_on=http:"+tokens[0]+"."+SETTING['DOMAIN']+":localhost:"+str(port)+":"+tokens[1]
+    command = PYTHON_DEFAULT+"./pagekite.py --clean"+frontend+service_on
+    args = shlex.split(command)
+    p = subprocess.Popen(args)
+    print('Initial TUNNEL for ['+tokens[0]+']: OK')
+  else:
+    print('Invalid token')
+
+def main():
+  pass
+
+if __name__ == '__main__':
+  main()
