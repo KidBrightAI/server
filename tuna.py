796189a36e37f8e696f825e0deb467fbe53854b0
Ridnarong Promya <ridnarongp@yahoo.com>
2023-01-25 21:11:08 +0700

    Fix error

--- tuna.py
+++ tuna.py
@@ -3,15 +3,15 @@ import sys
 
 PYTHON_DEFAULT = 'python'+str(sys.version_info[0])
 SETTING = {
-  'DOMAIN': 'wot.mecacloud.top'
+  'DOMAIN': 'kidbright.wot.mecacloud.top'
 }
 
 def run_tuna(port,token):
   tokens = token.split(':')
   if len(tokens) == 2:
-    frontend = " --frontend="+tokens[0]+":80"
+    frontend = " --frontend="+SETTING['DOMAIN']+":80"
     service_on = " --service_on=http:"+tokens[0]+"."+SETTING['DOMAIN']+":localhost:"+str(port)+":"+tokens[1]
-    command = PYTHON_DEFAULT+"./pagekite.py --clean"+frontend+service_on
+    command = PYTHON_DEFAULT+" ./pagekite.py --clean"+frontend+service_on
     args = shlex.split(command)
     p = subprocess.Popen(args)
     print('Initial TUNNEL for ['+tokens[0]+']: OK')
