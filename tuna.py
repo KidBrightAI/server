77db8ecead014291ea9e828e61b5421f1c27d49f
Ridnarong Promya <ridnarongp@yahoo.com>
2023-01-26 11:20:59 +0700

    Fix domain

--- tuna.py
+++ tuna.py
@@ -3,14 +3,14 @@ import sys
 
 PYTHON_DEFAULT = 'python'+str(sys.version_info[0])
 SETTING = {
-  'DOMAIN': 'kidbright.wot.mecacloud.top'
+  'DOMAIN': 'wot.mecacloud.top'
 }
 
 def run_tuna(port,token):
   tokens = token.split(':')
   if len(tokens) == 2:
-    frontend = " --frontend="+SETTING['DOMAIN']+":80"
-    service_on = " --service_on=http:"+tokens[0]+"."+SETTING['DOMAIN']+":localhost:"+str(port)+":"+tokens[1]
+    frontend = " --frontend="+tokens[0]+"."+SETTING['DOMAIN']+":80"
+    service_on = " --service_on=http:www."+tokens[0]+"."+SETTING['DOMAIN']+":localhost:"+str(port)+":"+tokens[1]
     command = PYTHON_DEFAULT+" ./pagekite.py --clean"+frontend+service_on
     args = shlex.split(command)
     p = subprocess.Popen(args)
